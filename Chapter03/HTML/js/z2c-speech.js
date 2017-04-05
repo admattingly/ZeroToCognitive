b991dc8da7de474a929c4480aff4aa79d9388de1
Andrew Mattingly <andrew@mattingly.com>
2017-04-05 11:33:04 +1000

    It works

--- Chapter03/HTML/js/z2c-speech.js
+++ Chapter03/HTML/js/z2c-speech.js
@@ -19,5 +19,41 @@
 
 function initPage ()
 {
+  var _mic = $('#microphone'); var _stop = $("#stop");
+    _mic.addClass("mic_enabled");
+    _stop.addClass("mic_disabled");
 
+  _mic.on("click", function ()
+    {
+      var _className = this.className;
+      if(this.className == "mic_enabled")
+      {
+        _mic.addClass("mic_disabled");
+        _mic.removeClass("mic_enabled");
+        _stop.addClass("mic_enabled");
+        _stop.removeClass("mic_disabled");
+        $.when($.get('/api/speech-to-text/token')).done(
+          function (token) {
+            stream = WatsonSpeech.SpeechToText.recognizeMicrophone({
+               token: token,
+               outputElement: '#speech' // CSS selector or DOM Element
+             });
+            stream.on('error', function(err) { 
+				console.log("Got an error in recognizeMicrophone");
+				console.log(err); });
+          });
+        }
+      });
+
+  _stop.on("click",  function() {
+          console.log("Stopping text-to-speech service...");
+          if (stream != undefined) {
+			  console.log("Issuing stream.stop()");
+			  stream.stop(); 
+			}
+          _mic.addClass("mic_enabled");
+          _mic.removeClass("mic_disabled");
+          _stop.addClass("mic_disabled");
+          _stop.removeClass("mic_enabled");
+        });
 }
